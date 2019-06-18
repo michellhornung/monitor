2ec45c9efda054b2064be0ce42d8009102cf98fd
Michell Hornung <michellhornung@gmail.com>
2019-06-18 18:23:04 -0300

    Started but not finished - Oracle view.

--- lib/pages/oracle.dart
+++ lib/pages/oracle.dart
@@ -12,6 +12,37 @@ class _OracleState extends State<Oracle> {
       appBar: AppBar(
         title: Text('Oracle'),
       ),
+      body: Container(
+        child: GridView.count(
+            crossAxisCount: 2,
+            crossAxisSpacing: 8.0,
+            mainAxisSpacing: 5.0,
+            padding: EdgeInsets.all(30.0),
+            children: List.generate(6, (index) {
+              return Container(
+                padding: EdgeInsets.all(30.0),
+                child: Center(
+                  child: GridTile(
+                    footer: Text(
+                      'Item $index',
+                      textAlign: TextAlign.center,
+                    ),
+                    header: Text(
+                      'SubItem $index',
+                      textAlign: TextAlign.center,
+                    ),
+                    child: Icon(Icons.data_usage,
+                        size: 40.0, color: Colors.white30),
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.grey[400],
+                  borderRadius: BorderRadius.circular(200.0),
+                ),
+                margin: EdgeInsets.all(1.0),
+              );
+            })),
+      ),
     );
   }
 }
