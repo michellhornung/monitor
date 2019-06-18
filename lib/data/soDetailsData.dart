a6be51b14962d981d628792c4f992dacbaff8a7d
Michell Hornung <michellhornung@gmail.com>
2019-06-18 13:14:22 -0300

    SO details done.

---
+++ lib/data/soDetailsData.dart
@@ -0,0 +1,47 @@
+import 'package:monitor/model/soDetailsModel.dart';
+
+final List<String> soDetailsColumns = [
+  'TOP',
+  'TASKS',
+  'CPU',
+  'MEM',
+  'SWAP',
+];
+
+final List<SODetails> soDetails = [
+  SODetails(
+    top: '15:51:25',
+    tasks: '272 total 1 running',
+    cpu: '0.8%us',
+    mem: '1645972k total',
+    swap: '7340028k total',
+  ),
+  SODetails(
+    top: '15:52:25',
+    tasks: '272 total 1 running',
+    cpu: '0.8%us',
+    mem: '1645972k total',
+    swap: '7340028k total',
+  ),
+  SODetails(
+    top: '15:53:25',
+    tasks: '272 total 1 running',
+    cpu: '0.8%us',
+    mem: '1645972k total',
+    swap: '7340028k total',
+  ),
+  SODetails(
+    top: '15:54:25',
+    tasks: '272 total 1 running',
+    cpu: '0.8%us',
+    mem: '1645972k total',
+    swap: '7340028k total',
+  ),
+  SODetails(
+    top: '15:55:25',
+    tasks: '272 total 1 running',
+    cpu: '0.8%us',
+    mem: '1645972k total',
+    swap: '7340028k total',
+  ),
+];
---
+++ lib/pages/soDetails.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:monitor/widgets/dataTableSODetails.dart';
+
+class SODetails extends StatefulWidget {
+  @override
+  _SODetailsState createState() => _SODetailsState();
+}
+
+class _SODetailsState extends State<SODetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Detalhes SO'),
+      ),
+      body: Container(
+        child: GridView.count(
+            crossAxisCount: 1,
+            children: List.generate(100, (index) {
+              return Container(
+                padding: EdgeInsets.all(10.0),
+                child: Center(
+                  child: GridTile(
+                    header: Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 0.1,
+                        horizontal: 90.0,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.orange,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Text(
+                        'alive $index',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          letterSpacing: 0.5,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    child: DataTableSODetails(),
+                  ),
+                ),
+                color: Colors.grey[300],
+                margin: EdgeInsets.all(8.0),
+              );
+            })),
+      ),
+    );
+  }
+}
