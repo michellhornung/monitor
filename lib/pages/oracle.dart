bc84bd77b51c0e7606aee126ff56662a0994ab2e
Michell Hornung <michellhornung@gmail.com>
2019-06-19 17:30:37 -0300

    Oracle view.

--- lib/pages/oracle.dart
+++ lib/pages/oracle.dart
@@ -12,36 +12,81 @@ class _OracleState extends State<Oracle> {
       appBar: AppBar(
         title: Text('Oracle'),
       ),
-      body: Container(
-        child: GridView.count(
-            crossAxisCount: 2,
-            crossAxisSpacing: 8.0,
-            mainAxisSpacing: 5.0,
-            padding: EdgeInsets.all(30.0),
-            children: List.generate(6, (index) {
-              return Container(
-                padding: EdgeInsets.all(30.0),
-                child: Center(
-                  child: GridTile(
-                    footer: Text(
-                      'Item $index',
-                      textAlign: TextAlign.center,
-                    ),
-                    header: Text(
-                      'SubItem $index',
-                      textAlign: TextAlign.center,
-                    ),
-                    child: Icon(Icons.data_usage,
-                        size: 40.0, color: Colors.white30),
-                  ),
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.grey[400],
-                  borderRadius: BorderRadius.circular(200.0),
-                ),
-                margin: EdgeInsets.all(1.0),
-              );
-            })),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: GridView.count(
+                  shrinkWrap: true,
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 8.0,
+                  mainAxisSpacing: 5.0,
+                  padding: EdgeInsets.all(30.0),
+                  children: List.generate(6, (index) {
+                    return Container(
+                      padding: EdgeInsets.all(30.0),
+                      child: Center(
+                        child: GridTile(
+                          footer: Text(
+                            'Item $index',
+                            textAlign: TextAlign.center,
+                          ),
+                          header: Text(
+                            'SubItem $index',
+                            textAlign: TextAlign.center,
+                          ),
+                          child: Icon(Icons.data_usage,
+                              size: 40.0, color: Colors.white30),
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[400],
+                        borderRadius: BorderRadius.circular(200.0),
+                      ),
+                      margin: EdgeInsets.all(1.0),
+                    );
+                  })),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Container(
+              child: GridView.count(
+                  shrinkWrap: true,
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 8.0,
+                  mainAxisSpacing: 5.0,
+                  padding: EdgeInsets.all(30.0),
+                  children: List.generate(2, (index) {
+                    return Container(
+                      padding: EdgeInsets.all(30.0),
+                      child: Center(
+                        child: GridTile(
+                          footer: Text(
+                            'Item $index',
+                            textAlign: TextAlign.center,
+                          ),
+                          header: Text(
+                            'SubItem $index',
+                            textAlign: TextAlign.center,
+                          ),
+                          child: Icon(Icons.data_usage,
+                              size: 40.0, color: Colors.white30),
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.green[400],
+                        //borderRadius: BorderRadius.circular(200.0),
+                      ),
+                      margin: EdgeInsets.all(1.0),
+                    );
+                  })),
+            ),
+          ],
+        ),
       ),
     );
   }
